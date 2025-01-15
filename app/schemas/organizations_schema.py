23bbfae5ffcb3b987a4fe4eb3737db303d54d9b1
Aditya-Sakpal <aditya.as@somaiya.edu>
2025-01-15 16:37:15 +0530

    changes in the request schema for adding users

--- app/schemas/organizations_schema.py
+++ app/schemas/organizations_schema.py
@@ -1,12 +1,64 @@
 from pydantic import BaseModel, HttpUrl
-from typing import Optional, Dict, Any
+from typing import Optional, List, Dict, Any
+
 
 class HttpRequest(BaseModel):
     client_ip: str
     user_agent: str
 
+
 class EventAttributes(BaseModel):
     http_request: HttpRequest
+
+
+class Organization(BaseModel):
+    admin_delete_enabled: bool
+    created_at: int
+    created_by: Optional[str] 
+    has_image: bool
+    id: str
+    image_url: Optional[HttpUrl]
+    logo_url: Optional[HttpUrl]
+    max_allowed_memberships: int
+    members_count: int
+    name: str
+    object: str
+    pending_invitations_count: int
+    public_metadata: Dict[str, Any]
+    slug: str
+    updated_at: int
+
+
+class PublicUserData(BaseModel):
+    first_name: str
+    has_image: bool
+    identifier: str
+    image_url: Optional[HttpUrl]
+    last_name: str
+    profile_image_url: Optional[HttpUrl]
+    user_id: str
+
+
+class OrganizationMembershipData(BaseModel):
+    created_at: int
+    id: str
+    object: str
+    organization: Organization
+    permissions: List[str]
+    public_metadata: Dict[str, Any]
+    public_user_data: PublicUserData
+    role: str
+    role_name: str
+    updated_at: int
+
+
+class AddUserToOrganizationRequest(BaseModel):
+    data: OrganizationMembershipData
+    event_attributes: EventAttributes
+    object: str
+    timestamp: int
+    type: str
+
 
 class OrganizationData(BaseModel):
     created_at: int
@@ -20,6 +72,7 @@ class OrganizationData(BaseModel):
     slug: str
     updated_at: int
 
+
 class CreateOrganizationRequest(BaseModel):
     data: OrganizationData
     event_attributes: EventAttributes
@@ -27,46 +80,12 @@ class CreateOrganizationRequest(BaseModel):
     timestamp: int
     type: str
 
-class Organization(BaseModel):
-    created_at: int
-    created_by: str
-    id: str
-    image_url: Optional[HttpUrl]
-    logo_url: Optional[HttpUrl]
-    name: str
-    object: str
-    public_metadata: Dict[str, Any]
-    slug: str
-    updated_at: int
-
-class PublicUserData(BaseModel):
-    first_name: str
-    identifier: str
-    image_url: Optional[HttpUrl]
-    last_name: str
-    profile_image_url: Optional[HttpUrl]
-    user_id: str
-
-class OrganizationMembershipData(BaseModel):
-    created_at: int
-    id: str
-    object: str
-    organization: Organization
-    public_user_data: PublicUserData
-    role: str
-    updated_at: int
-
-class AddUserToOrganizationRequest(BaseModel):
-    data: OrganizationMembershipData
-    event_attributes: EventAttributes
-    object: str
-    timestamp: int
-    type: str
 
 class DeleteOrganizationData(BaseModel):
     deleted: bool
     id: str
     object: str
+
 
 class DeleteOrganizationRequest(BaseModel):
     data: DeleteOrganizationData
@@ -74,7 +93,8 @@ class DeleteOrganizationRequest(BaseModel):
     object: str
     timestamp: int
     type: str
-    
+
+
 class UpdateOrganizationRequest(BaseModel):
     data: OrganizationData
     event_attributes: EventAttributes
--- main.py
+++ main.py
@@ -46,4 +46,4 @@ app.include_router(formats_router.router, prefix="/api/v1")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=3003)
+    uvicorn.run(app, host="0.0.0.0", port=3003,log_level="debug")
